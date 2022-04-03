49c32231bab8e5a1abacc8cafcc8457098c76089
Benchaphol <6306021620085@fitm.kmutnb.ac.th>
2022-04-03 21:12:20 +0700

    update

--- login.js
+++ login.js
@@ -1,3 +1,17 @@
 function show_pwd(opt){
     form.pwd.setAttribute('type', opt? 'text' : 'password');
 }
+
+let username = ["admin"]
+let password = ["admin"]
+
+function check(){
+    var log_user = document.getElementById("user").value;
+    var log_pwd = document.getElementById("pwd").value;
+    for(var i=0; i < username.length; i++){
+        if((log_user == username[i]) && (log_pwd == password[i])) {
+            window.location = "room_list.html";
+        }
+        else alert("username or password incorrect")
+    }
+}
