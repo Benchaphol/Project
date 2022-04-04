9903cea2a0a8b1bcbd761fd448ab1665fd875092
Benchaphol <6306021620085@fitm.kmutnb.ac.th>
2022-04-04 16:14:29 +0700

    update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let username = ["admin"]
 let password = ["admin"]
 let noti_box  = true;
 
+//Check password and user in page login.html
 function check(){
     var log_user = document.getElementById("user").value;
     var log_pwd = document.getElementById("pwd").value;
@@ -21,6 +22,7 @@ function check(){
     if(noti_box) alert("You account is incorrent!");
 }
 
+// Show password in page login.html
 function show_pwd(opt){
     form.pwd.setAttribute('type', opt? 'text' : 'password');
 }
