45ab53f7eba013fc40d462fcfe0e9f9a9844bc95
Benchaphol <6306021620085@fitm.kmutnb.ac.th>
2022-04-04 16:03:56 +0700

    update

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let username = ["admin"]
 let password = ["admin"]
+let noti_box  = true;
 
 function check(){
     var log_user = document.getElementById("user").value;
-    var log_pwd = document.getElementById("pwd").value; 
+    var log_pwd = document.getElementById("pwd").value;
+    if((log_user == "") &&(log_pwd == "")) noti_box = false;
     for(var i=0; i < username.length; i++){
-        if((log_user == "admin") && (log_pwd == "admin")) window.location = "room_admin.html";
-        else if((log_user == username[i]) && (log_pwd == password[i])) window.location = "room_student.html";
-        else alert("You account is incorrect!");
+        if((log_user == "admin") && (log_pwd == "admin")){
+            window.location = "room_admin.html";
+            noti_box = false;
+            break;
+        }
+        else if((log_user == username[i]) && (log_pwd == password[i])){
+            window.location = "room_student.html";
+            noti_box = false;
+            break;
+        }
     }
+    if(noti_box) alert("You account is incorrent!");
 }
 
 function show_pwd(opt){
